c22c629d6fa31e285a60c997d4f990ee7f20d95c
Emmanuel-dv <emmanuel.zacariasbenitez@gmail.com>
2026-06-03 23:19:24 +0200

    Correcion: Arreglo DataLoader

--- src/main/java/org/example/examenjparecuperacion/repository/NivelRepository.java
+++ src/main/java/org/example/examenjparecuperacion/repository/NivelRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NivelRepository extends JpaRepository<Long, Nivel> {
+public interface NivelRepository extends JpaRepository<Nivel, Long> {
 }
